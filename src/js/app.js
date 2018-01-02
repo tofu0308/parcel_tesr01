150c9f95c708f0443fa58ff85af23e892ad81c7a
tofu0308 <isisaka2000@gmail.com>
2018-01-03 00:20:30 +0900

    parsel test

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import	'../../src/css/style.scss';
+import add from './modules/addition-calculator.js';
+
+const number1 = 100;
+const number2 = 200;
+const total = add(number1, number2);
+
+console.log(total);
+
